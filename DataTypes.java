23c9821f2b4b745e5ea695db8758befeeb45294e
Michael07080 <parisemichael26@gmail.com>
2025-12-03 13:29:30 +0000

    First commit

---
+++ DataTypes.java
@@ -0,0 +1,23 @@
+// Java is a strongly typed language; you hvae to define/declare a variable
+// you have to decalare its data type
+
+class DataTypeExamples {
+
+    int myInt = 42;
+    double myDouble = 189.75;
+    char  myCharacter = 'Q';
+    boolean myBoolean = false;
+    
+
+}
+
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        DataTypeExamples dt = new DataTypeExamples();
+
+        System.out.println("myInt: " + dt.myInt);
+
+    }
+}
