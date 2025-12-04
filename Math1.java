bb9a9482536ef6dd17047fa4d608d498bce66e50
Michael07080 <parisemichael26@gmail.com>
2025-12-04 14:38:55 +0000

    1st commit

---
+++ Math1.java
@@ -0,0 +1,56 @@
+
+class Calculator {
+
+    // write fuctions for addition, multiplication, division, etc
+    int add(int a, int b) {
+        return a + b;
+
+    }
+
+    int subtract(int a, int b) {
+        return a - b;
+    }
+
+
+    int multiply(int a, int b) {
+        return a * b;
+    }
+
+    int divide(int a, int b) {
+        return a / b;
+
+    }
+    // Method overriding -- polymorphism
+    double divide(double a, double b) {
+        return a /b;
+    }
+
+
+    int modulo(int a, int b) {
+        return a % b;
+
+    }
+
+
+
+}
+
+
+public class Math1 {
+
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+        System.out.println(calc.subtract(10, 5));
+        System.out.println(calc.add(10, 5));
+        System.out.println(calc.divide(10, 5));
+        System.out.println(calc.divide(10.6, 5.3));
+        System.out.println(calc.multiply(10, 5));
+        System.out.println(calc.modulo(10, 5));
+
+
+    }
+
+
+}
