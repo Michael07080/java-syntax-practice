27db40e7878d320e4cb7b597bcfd7c37a326f6b4
Michael07080 <parisemichael26@gmail.com>
2026-02-24 21:15:07 +0000

    final commit

--- GetSetTester.java
+++ GetSetTester.java
@@ -7,7 +7,17 @@ public static void main(String[] args) {
 
     GetSet player1 = new GetSet(); 
 
+    System.out.println("Enter a username");
+    String username2 = input.nextLine();
 
+
+    System.out.println("Enter a id");
+    int id2 = input.nextInt();
+
+
+    
+
+   
 
     //System.out.println(player1.username);
     System.out.println(player1.getUsername());
@@ -24,6 +34,37 @@ public static void main(String[] args) {
     System.out.println(player1.getRank());
     player1.setRank("gold");
     System.out.println(player1.getRank());
+
+    System.out.println(player1.getid2());
+    player1.setid2(id2);
+    System.out.println(player1.getid2());
+
+    System.out.println(player1.getUsername2());
+    player1.setUsername2(username2);
+    System.out.println(player1.getUsername2());
+
+
+
+
+
+    if (player1.getRank().equals("gold")) {
+            System.out.println("You are alsmot platnum");
+        } else {
+            System.out.println("you are not platnum");
+        } 
+    
+    if (player1.getRank().equals("platnum")) {
+            System.out.println("You are platnum");
+        } else {
+            System.out.println("you are not platnum");
+        } 
+    
+
+
+
+
+    
+
 }
 
 
@@ -35,6 +76,10 @@ class GetSet {
     private String username = "Drewmickymouse";
     private int id = 45;
     private String rank = "platnum";
+    private String username2 = " ";
+    private int id2 = ' ';
+    
+ 
 
 
     //getter
@@ -46,6 +91,17 @@ class GetSet {
     public String getUsername(){
         return username;
     }
+
+
+    public int getid2(){
+        return id;
+    }
+
+    //getter
+    public String getUsername2(){
+        return username;
+    }
+
 
     //getter
     public String getRank(){
@@ -66,4 +122,12 @@ class GetSet {
         this.rank = newrank;
     }
 
-}
+    public void setUsername2(String newusername2){
+        this.username = newusername2;
+    }
+
+    //setter
+    public void setid2(int newid2){
+        this.id = newid2;
+
+}}
